e32d205c615ad5aa752c140db12a293974752c20
Sumedha <sumedha@CND3021X8Q-HP-MFLK.medfuturesl.local>
2023-12-08 13:06:12 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Typography } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+     <Typography component='h1'>Test App</Typography>
+    </div>
+  );
+}
+
+export default App;
